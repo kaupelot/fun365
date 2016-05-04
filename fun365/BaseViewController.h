529b9146b43887f93af27d0ecff3bc815e55f046
walt zeng <kaupelot@gmail.com>
2016-05-05 02:44:15 +0800

    create base

---
+++ fun365/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  fun365
+//
+//  Created by walt zeng on 16/5/4.
+//  Copyright © 2016年 曾旺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
